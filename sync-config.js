c47b92dff73e92b393a42e395709e7c705b7f9ec
controlesmineras <controlesminerask@gmail.com>
2026-09-15 07:48:18 -0500

    Conectar SK Web con la nueva implementación del servicio central

--- sync-config.js
+++ sync-config.js
@@ -1,2 +1,2 @@
 // URL del puente privado SK Web -> Google Drive.
-window.SK_SYNC_CONFIG={apiUrl:'https://script.google.com/macros/s/AKfycbzLgJxcix535p9zglHQ1CFoKeABO_xU7usE3K60bwK7bLeZPmIjXBbp_GjaNWrVg-Sx/exec',apiKey:'SKW-PLAZA-2026-9F7M4Q2X8R6K3V5N',intervalMs:60000};
+window.SK_SYNC_CONFIG={apiUrl:'https://script.google.com/macros/s/AKfycbwq1W8ovVivygL9oWnuA8dDoPmwdt6hoGBDacDWwUq-saRpbVDJai_U5kNsUrUavByD/exec',apiKey:'SKW-PLAZA-2026-9F7M4Q2X8R6K3V5N',intervalMs:60000};
